00abf657f6afc125700f71f5684d1d7f70ddc4b1
Judson Stevens <Judson.Stevens@gmail.com>
2018-07-25 11:27:08 -0600

    Refactoring, WIP

--- spec/features/visitor_can_search_for_a_word_and_see_examples_of_how_to_use_it_spec.rb
+++ spec/features/visitor_can_search_for_a_word_and_see_examples_of_how_to_use_it_spec.rb
@@ -12,7 +12,7 @@ describe 'Visitor' do
       expect(page).to have_content('So mindfulness of drinking is already one kind of enlightenment.')
       expect(page).to have_content('Apart from the hours of sound sleep, you should try to maintain continuous mindfulness throughout your waking hours.')
       expect(page).to_not have_content('At such times, mindfulness of the practice of patience and the application of certain techniques will help us to continue generating this attitude.')
-      expect(page).to_not have_content('ry to develop deep concentration through the awareness of the breath which is called the mindfulness of the breath or you can say mindfulness of the body.')
+      expect(page).to_not have_content('Try to develop deep concentration through the awareness of the breath which is called the mindfulness of the breath or you can say mindfulness of the body.')
     end
   end
 end
--- spec/requests/game_requests_spec.rb
+++ spec/requests/game_requests_spec.rb
@@ -14,6 +14,8 @@ describe 'User', type: :request do
   it 'can send a get request to /api/v1/games/1 and get a response in JSON' do
     get "/api/v1/games/#{Game.last.id}"
 
+    expect(response.body.class).to eq(String)
+
     data = JSON.parse(response.body)
 
     expect(data['game_id']).to eq(Game.last.id)
@@ -24,10 +26,11 @@ describe 'User', type: :request do
     expect(data['scores'].last['score']).to eq(16)
   end
 
-  it 'can send a post request to /api/v1/games/1/plays and receive a 201 Created Response along with JSON' do
+  it 'can send a post request to /api/v1/games/1/plays and receive a 201 response code along with JSON' do
     post "/api/v1/games/#{Game.last.id}/plays", params: { plays: { user_id: "#{User.first.id}", word: "at" } }
-    
+
     expect(response.status).to eq(201)
+    expect(response.body.class).to eq(String)
 
     get "/api/v1/games/#{Game.last.id}"
 
